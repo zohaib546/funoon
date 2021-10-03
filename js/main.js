046ea14f52e0122202a91e362ff373899c606503
Zohaib Ashraf <zohaibashraf546@gmail.com>
2021-10-03 17:01:28 +0500

    landing page web & mobile responsive complete

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(".video-carousel").owlCarousel({
 	},
 });
 
-$(".drama-carousel").owlCarousel({
+$(".movie-carousel").owlCarousel({
 	loop: true,
 	margin: 5,
 	nav: true,
@@ -29,12 +29,107 @@ $(".drama-carousel").owlCarousel({
 	responsive: {
 		0: {
 			items: 3,
+			nav: false,
 		},
 		600: {
 			items: 6,
 		},
 		1000: {
 			items: 9,
+		},
+		1920: {
+			items: 12,
+		},
+	},
+});
+
+$(".exclusive-carousel").owlCarousel({
+	loop: true,
+	margin: 5,
+	nav: true,
+	mouseDrag: false,
+	dots: false,
+	navText: [
+		"<span class='carousel-nav-left'><i class='fa fa-chevron-left'></i></span>",
+		"<span class='carousel-nav-right'><i class='fa fa-chevron-right'></i></span>",
+	],
+	responsive: {
+		0: {
+			items: 2,
+			nav: false,
+			stagePadding: 50,
+		},
+		600: {
+			items: 5,
+		},
+		1000: {
+			items: 6,
+		},
+		1920: {
+			items: 9,
+		},
+	},
+});
+
+$(".top-carousel").owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	mouseDrag: false,
+	stagePadding: 80,
+	dots: false,
+	navText: [
+		"<span class='carousel-nav-left'><i class='fa fa-chevron-left'></i></span>",
+		"<span class='carousel-nav-right'><i class='fa fa-chevron-right'></i></span>",
+	],
+	responsive: {
+		0: {
+			items: 2,
+			nav: false,
+			stagePadding: 30,
+		},
+		600: {
+			items: 3,
+		},
+		1000: {
+			items: 4,
+			stagePadding: 50,
+		},
+		1920: {
+			items: 6,
+		},
+		2000: {
+			items: 8,
+		},
+	},
+});
+
+$(".resume-carousel").owlCarousel({
+	loop: true,
+	margin: 5,
+	nav: true,
+	mouseDrag: false,
+	stagePadding: 0,
+	dots: false,
+	navText: [
+		"<span class='carousel-nav-left'><i class='fa fa-chevron-left'></i></span>",
+		"<span class='carousel-nav-right'><i class='fa fa-chevron-right'></i></span>",
+	],
+	responsive: {
+		0: {
+			items: 1,
+			nav: false,
+			stagePadding: 50,
+		},
+		600: {
+			items: 3,
+		},
+		1000: {
+			items: 4,
+			stagePadding: 50,
+		},
+		1920: {
+			items: 6,
 		},
 	},
 });
