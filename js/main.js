61ea1772476dbb1fb3837243420b7cf638cf354c
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-02-05 16:49:28 +0500

    animation on scroll added

--- js/main.js
+++ js/main.js
@@ -905,3 +905,25 @@ function handleMovieTranslation() {
 		});
 	}
 }
+
+// let callback = (entries, observer) => {
+// 	entries.forEach((entry) => {
+// 		// Each entry describes an intersection change for one observed
+// 		// target element:
+// 		//   entry.boundingClientRect
+// 		//   entry.intersectionRatio
+// 		//   entry.intersectionRect
+// 		//   entry.isIntersecting
+// 		//   entry.rootBounds
+// 		//   entry.target
+// 		//   entry.time
+// 	});
+// };
+
+// let options = {
+// 	root: document.querySelector("#scrollArea"),
+// 	rootMargin: "0px",
+// 	threshold: 1.0,
+// };
+
+// let observer = new IntersectionObserver(callback, options);
